ec57dcd3b37f1ea7570da9ddc8a1e43012b92472
yasuaki-hikishima <yasuaki_hikishima@waku-2.com>
2014-12-26 09:21:58 +0900

    コメント削除

--- js/app.js
+++ js/app.js
@@ -2,17 +2,36 @@
 * AngularJS app.js
 */
 
-// function MainCtrl($scope) {
-//   $scope.people = [
-//     { firstName: 'Kazuhito', lastName: 'Hokamura' },
-//     { firstName: 'Takeshi',  lastName: 'Takatsudo' },
-//     { firstName: 'Akihiro',  lastName: 'Oyamada' },
-//     { firstName: 'Kazuhori',  lastName: 'Tokuda' },
-//     { firstName: 'Yukihisa',  lastName: 'Yamada' },
-//   ];
-// }
-
 var app = {};
+
+// functions
+function TodoCtrl($scope) {
+  $scope.todos = [
+    {text:'AngularJSの学習', done:true},
+    {text:'AngularJSのアプリケーション構築', done:false}];
+
+  $scope.addTodo = function() {
+    $scope.todos.push({text:$scope.todoText, done:false});
+    $scope.todoText = '';
+  };
+
+  $scope.remaining = function() {
+    var count = 0;
+    angular.forEach($scope.todos, function(todo) {
+      count += todo.done ? 0 : 1;
+    });
+    return count;
+  };
+
+  $scope.archive = function() {
+    var oldTodos = $scope.todos;
+    $scope.todos = [];
+    angular.forEach(oldTodos, function(todo) {
+      if (!todo.done) $scope.todos.push(todo);
+    });
+  };
+}
+
 
 (function(app){
   app = angular.module('myApp', ['ngResource']);
@@ -40,27 +59,3 @@ var app = {};
 
 
 })(app || (app = {}));
-
-function TodoCtrl($scope) {
-  $scope.todos = [
-    {text:'AngularJSの学習', done:true},
-    {text:'AngularJSのアプリケーション構築', done:false}];
-  $scope.addTodo = function() {
-    $scope.todos.push({text:$scope.todoText, done:false});
-    $scope.todoText = '';
-  };
-  $scope.remaining = function() {
-    var count = 0;
-    angular.forEach($scope.todos, function(todo) {
-      count += todo.done ? 0 : 1;
-    });
-    return count;
-  };
-  $scope.archive = function() {
-    var oldTodos = $scope.todos;
-    $scope.todos = [];
-    angular.forEach(oldTodos, function(todo) {
-      if (!todo.done) $scope.todos.push(todo);
-    });
-  };
-}
